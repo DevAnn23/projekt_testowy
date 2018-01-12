1d0cbd787b9c9d3a140cbbc1dfad6d60ccb1ece7
DevAnn23 <abanaszek2@gmail.com>
2018-01-12 11:51:51 +0100

    invalid form

--- script.js
+++ script.js
@@ -29,4 +29,59 @@ window.onload = function () {
 
 	myFunction();
 
+	function validForm() {
+		var labelAdd = document.querySelector('#labelAdd');
+		var phoneField = document.querySelector('.form').phone;
+		var emailField = document.querySelector('.form').email;
+		var messageField = document.querySelector('.form').message;
+		var additionField = document.querySelector('.form').addition;
+		var formButton = document.querySelector('.form-button');
+		var tooltipSend = document.getElementById('tooltipSend');
+		var tooltipNumber = document.getElementById('tooltipNumber');
+		var tooltipAdd = document.getElementById('tooltipAdd');
+		var isOk = true;
+		var intAddNumber = parseInt(additionField.value);
+
+		function addNumber() {
+			var num1 = Math.floor(Math.random() * 10);
+			var num2 = Math.floor(Math.random() * 10);
+			console.log(num1 + num2);
+
+			labelAdd.innerHTML = num1 + "+" + num2;
+
+			additionField.onkeyup = function (e) {
+				if (parseInt(additionField.value) === num1 + num2 && parseInt(additionField.value) != null) {
+					console.log("jest ok");
+					isOk = true;
+
+				} else {
+					isOk = false;
+					console.log("jest none");
+				}
+			}
+		}
+		addNumber();
+		phoneField.onkeyup = function (e) {
+			if (this.value === "" || isNaN(this.value)) {
+				tooltipNumber.innerHTML = "Invalid number";
+				isOk = false;
+
+			} else {
+				tooltipNumber.innerHTML = "";
+				isOk = true;
+			}
+		}
+		formButton.onclick = function (e) {
+			if (!isOk) {
+				e.preventDefault();
+				tooltipSend.innerHTML = "Incorrect data";
+			} else {
+				tooltipSend.innerHTML = "";
+			}
+
+		}
+
+	}
+	validForm();
+
 }
